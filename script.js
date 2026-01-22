0ec42ad0468c6246b4b52b6ec7c0990e56b2df23
palkinn710 <palkinjain710@gmail.com>
2026-01-22 11:52:38 +0530

    refactor: comment out event listener code and clean up script

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const container=document.querySelector(".container")
-container.addEventListener("click",()=>{console.log("div")},true)
+// const container=document.querySelector(".container")
+// container.addEventListener("click",()=>{console.log("div")},true)
 
-const button=document.querySelector("button")
-button.addEventListener("click",()=>{console.log("button")},true)
+// const button=document.querySelector("button")
+// button.addEventListener("click",()=>{console.log("button")},true)
 
 
-const outer=document.querySelector(".outer")
-outer.addEventListener("click",()=>{console.log("outer div")},true)
+// const outer=document.querySelector(".outer")
+// outer.addEventListener("click",()=>{console.log("outer div")},true)
 
-console.log("new code")
+// console.log("new code")
+
+let a=123456
+var b=675899
+console.log(a)
+console.log(b)
+function print(){
+    console.log("inside function")
+
+
+}
+print()
+
+
