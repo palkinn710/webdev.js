ece13513c7410a934c2034dd8e648e5897eae018
palkinn710 <palkinjain710@gmail.com>
2026-01-22 11:20:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const container=document.querySelector(".container")
+container.addEventListener("click",()=>{console.log("div")},true)
+
+const button=document.querySelector("button")
+button.addEventListener("click",()=>{console.log("button")},true)
+
+
+const outer=document.querySelector(".outer")
+outer.addEventListener("click",()=>{console.log("outer div")},true)
+
+console.log("new code")
