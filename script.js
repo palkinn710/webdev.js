4f32bb256b6c4ceb843c0cb987d7d1db57e538e9
palkinn710 <palkinjain710@gmail.com>
2026-01-28 12:41:02 +0530

    create element etc.

--- script.js
+++ script.js
@@ -21,9 +21,9 @@
 // }
 // print()    
 // global context starts from thr first and while ending the fiunction it starts closing from the last function.
-function first(){
-     second()
-}
+// function first(){
+//      second()
+// }
 
 
 // // edit
@@ -53,12 +53,40 @@ function first(){
 // console.log("second line")
 
 // timers
-setTimeout(()=>{
-    console.log("after 5 secs")
-},2*1000)
- const timerid= setInterval(()=>{console.log("setInterval")},1000)
- setTimeout(()=>{clearInterval(timerid) , 1000})
+// setTimeout(()=>{
+//     console.log("after 5 secs")
+// },2*1000)
+//  const timeridd= setInterval(()=>{console.log("setInterval")},1000)
+//  setTimeout(()=>{clearInterval(timerid) , 1000})
 //  take a var with starting val 1 and it will print value updated +1 eveytime and when it reaches 10 it should stop printing 
+console.log("hello world")
+console.log("hello world")
+// var count=1;
+// const timerid= setInterval(()=>{console.log(count)
+// count++}
+// ,1000)
+//  setTimeout(()=>{clearInterval(timerid) , 10000})
+//  let num =1;
+//  const id=setInterval(()=>{
+//     if (num===10)clearInterval(id)
+//         console.log(num)
+//     num+=1
+// },1000)
+const name=document.queryselector("#name")
+const btn=document.querySelector(".btn")
+const list=document.queryselector(".list")
+btn.addEventListener('click' ,()=>{
+    const li=document.createlement('li')
+    const dlt=document.createElement('button')
+    dlt.innertext="delete"
+    li.innertext=name.value;
+    dlt.addEventListener("click",()=>{
+        list.removechild(li)
+    })
+    list.appendchild(dlt)
+    list.appendchild(li)
+    name.value=""
 
-var count=1;
-while var1=
+})
+
+
