7173427207e936db3aed30d2b0dfbeea27d04184
palkinn710 <palkinjain710@gmail.com>
2026-02-03 14:34:35 +0530

    variables practice

---
+++ variables & data types.js
@@ -0,0 +1,21 @@
+// types of variables
+// three types let var const
+// datatypes
+
+// number
+// var a =  34;
+// var b = 78;
+// console.log(typeof (a));
+// // boolean
+var c= true;
+var d= false;
+console.log(typeof(c))
+// undefined
+var e;
+console.log(typeof (e))
+// null
+var f= null;
+console.log(typeof(f))
+// big integer
+var g= 123456788n;
+console.log(typeof (g))
